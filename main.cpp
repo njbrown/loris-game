2a7058ab7497f616e1fabce9037674c28d1b402f
Nicolas Brown <nclsbrwn@gmail.com>
2018-09-16 20:19:52 -0500

    Initializing sdl image

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include "SDL.h"
+#include "SDL_image.h"
+#include "loris\loris.hpp"
+
+
 
 int main(int argc, char *argv[])
 {
+
 	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window *window = SDL_CreateWindow(
@@ -12,6 +17,8 @@ int main(int argc, char *argv[])
 	480,
 	0
 	);
+
+	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG | IMG_INIT_TIF | IMG_INIT_WEBP);
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
